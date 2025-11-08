07350b296beb55233e8f374743609f12b9924f35
Tizaoui Aya <tizaouiaya715@gmail.com>
2025-11-08 21:44:02 +0100

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Tizaoui Aya',
+    author_email = 'tizaouiaya715@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
